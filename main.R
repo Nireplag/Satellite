cdfbe9ab5372169c3909ff12098a770c3b9f6f6b
Gabriel Galperin <galperin@alunos.utfpr.edu.br>
2021-05-22 15:51:20 -0300

    Random forest file and main update nomenclatures

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 # Initialize packages
 library(mlbench)
+library(caret)
 
 # load dataset
 data(Satellite)
@@ -7,5 +8,19 @@ data(Satellite)
 # build needed dataset 
 
 df <- Satellite[, c(17:20, 37)]
+
+# split train and test sets
+set.seed(9)
+index <- createDataPartition(df$classes, p=0.80, list = FALSE) 
+
+df_train <- df[index,]
+df_test <- df[-index,]
+
+# train and predict for random forest
+
+rf <- train(classes~., data = df_train, method = "rf")
+save(rf, file = "rf.RData")
+predictions_rf <- predict(rf, df_test)
+confusionMatrix(predictions_rf, df_test$classes)
 
 
